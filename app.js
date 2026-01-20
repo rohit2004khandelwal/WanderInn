d3d146f9e25738795baf966a25b6108ecce6bc3f
rohit2004khandelwal <rohit2004khandelwal@gmail.com>
2026-01-20 22:54:15 +0530

    added templates

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ async function main(){
 }
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));//parsing
 app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, '/public')));
